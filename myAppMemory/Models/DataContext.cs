d89ef9d0cdea986a913a94d65d127ac5908f83c2
igoryen <igor.yentaltsev@gmail.com>
2014-02-06 11:27:27 -0500

    Models/DataContext.cs: added from seed file

---
+++ myAppMemory/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace CodeFirstOne.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("name=DataContext") { }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Faculty> Faculty { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        public System.Data.Entity.DbSet<CodeFirstOne.ViewModels.StudentFull> StudentFulls { get; set; }
+    }
+}
