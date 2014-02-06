827cd4cb991b2c36e21b14ab860ab6ed0dc707d5
igoryen <igor.yentaltsev@gmail.com>
2014-02-06 11:34:47 -0500

    Models/Manager.cs: changed with seed file

--- myAppMemory/Models/Manager.cs
+++ myAppMemory/Models/Manager.cs
@@ -6,40 +6,39 @@ using System.Web.Mvc;
 
 namespace myAppMemory.Models {
   public class Manager {
-    public Manager() {
-      this.Students = (List<Student>)HttpContext.Current.Application["Students"];
-    }
-
+    DataContext dc = new DataContext();
     public Student getStudent(int? id) {
-      return Students.FirstOrDefault(n => n.Id == id);
+      return dc.Students.FirstOrDefault(n => n.Id == id);
     }
 
     public IEnumerable<Student> sortStudents() {
-      return this.Students.OrderBy(n => n.Id);
+      return dc.Students.OrderBy(n => n.Id);
     }
 
     public Student createStudent(Student stu) {
-      //stu.Id = Students.Last().Id + 1;
-      stu.Id = Students.Max(n => n.Id) + 1;
-      Students.Add(stu);
+      dc.Students.Add(stu);
+      dc.SaveChanges();
+
       return stu;
     }
 
     public Student createStudent(string fName, string lName, string pNumber, string sid) {
       var st = new Student(fName, lName, pNumber, sid);
-      st.Id = Students.Last().Id + 1;
 
-      Students.Add(st);
+      dc.Students.Add(st);
+      dc.SaveChanges();
 
       return st;
     }
     public Student editStudent(int id, string fName, string lName, string pNumber, string sid) {
-      var stu = Students.FirstOrDefault(b => b.Id == id);
+      var stu = dc.Students.FirstOrDefault(b => b.Id == id);
 
       stu.FirstName = fName;
       stu.LastName = lName;
       stu.Phone = pNumber;
       stu.StudentNumber = sid;
+
+      dc.SaveChanges();
 
       return stu;
     }
@@ -47,11 +46,8 @@ namespace myAppMemory.Models {
     //this is essentailly a ViewModel because the SelectList only carries the data needed to display a list control
     //don't do this until week two.
     public SelectList getSelectList() {
-      SelectList sl = new SelectList(Students, "Id", "StudentNumber");
+      SelectList sl = new SelectList(dc.Courses, "Id", "CourseCode");
       return sl;
     }
-
-
-    public List<Student> Students { get; set; }
   }
 }
