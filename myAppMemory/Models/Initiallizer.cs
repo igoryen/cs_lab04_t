4346424311ce3d0ed1f00febbfab13cceed394b7
igoryen <igor.yentaltsev@gmail.com>
2014-02-06 11:33:31 -0500

    Models/Initializer.cs: changed by a seed file

--- myAppMemory/Models/Initiallizer.cs
+++ myAppMemory/Models/Initiallizer.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace myAppMemory.Models {
-  public class Initiallizer {
-    public Initiallizer() {
-      this.Students = new List<Student>();
+  public class Initiallizer : DropCreateDatabaseAlways<DataContext> {
+    protected override void Seed(DataContext dc) {
+      Course int422 = new Course();
+      int422.CourseCode = "INT422";
+      int422.CourseName = "Windows Web Programming";
+
+
+      Course jac444 = new Course();
+      jac444.CourseName = "Java";
+      jac444.CourseCode = "JAC444";
 
       Student student = new Student();
       student.Id = 1;
@@ -14,8 +22,12 @@ namespace myAppMemory.Models {
       student.LastName = "Smith";
       student.Phone = "555-555-5555";
       student.StudentNumber = "011111111";
+      student.Courses.Add(jac444);
+      student.Courses.Add(int422);
 
-      Students.Add(student);
+      dc.Students.Add(student);
+      int422.Students.Add(student);
+      jac444.Students.Add(student);
       student = null;
 
       student = new Student();
@@ -24,8 +36,12 @@ namespace myAppMemory.Models {
       student.LastName = "Brown";
       student.Phone = "555-555-5555";
       student.StudentNumber = "011111112";
+      student.Courses.Add(jac444);
+      student.Courses.Add(int422);
 
-      Students.Add(student);
+      dc.Students.Add(student);
+      int422.Students.Add(student);
+      jac444.Students.Add(student);
       student = null;
 
       student = new Student();
@@ -34,17 +50,30 @@ namespace myAppMemory.Models {
       student.LastName = "Chen";
       student.Phone = "555-555-5555";
       student.StudentNumber = "011111113";
+      student.Courses.Add(jac444);
+      student.Courses.Add(int422);
 
-      Students.Add(student);
+      dc.Students.Add(student);
+      int422.Students.Add(student);
+      jac444.Students.Add(student);
       student = null;
 
       student = new Student("John", "Woo", "555-555-1234", "011111114");
       student.Id = 4;
+      student.Courses.Add(jac444);
+      student.Courses.Add(int422);
 
-      Students.Add(student);
+      dc.Students.Add(student);
+      int422.Students.Add(student);
+      jac444.Students.Add(student);
+      dc.Courses.Add(int422);
+      dc.Courses.Add(jac444);
 
+      Faculty fac = new Faculty("Peter", "McIntyre", "555-567-6789", "034234678");
+      fac.Courses.Add(jac444);
+      fac.Courses.Add(int422);
+      dc.Faculty.Add(fac);
+      dc.SaveChanges();
     }
-
-    public List<Student> Students { get; set; }
   }
 }
