0f55d3fc91b84aa70d1fcd3d39a60703095801da
igoryen <igor.yentaltsev@gmail.com>
2014-02-06 11:39:09 -0500

    Added more from seed file(2)

---
+++ myAppMemory/ViewModels/VM_Courses.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeFirstOne.ViewModels
+{
+    public class CourseForList
+    {
+        [Key]
+        public int CourseId { get; set; }
+        public string CourseCode { get; set; }
+    }
+}
--- myAppMemory/ViewModels/VM_Student.cs
+++ myAppMemory/ViewModels/VM_Student.cs
@@ -3,49 +3,77 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using CodeFirstOne.Models;
 
-namespace myAppMemory.ViewModels {
-  public class StudentsForList {
-    [Key]
-    public int StudentId { get; set; }
+namespace CodeFirstOne.ViewModels
+{
+    public class StudentsForList
+    {
+        [Key]
+        public int StudentId { get; set; }
 
-    [Required]
-    [RegularExpression("^[0][0-9]{8}$", ErrorMessage = "0 followed by 8 digits")]
-    public string StudentNumber { get; set; }
-  }
+        [Required]
+        [RegularExpression("^[0][0-9]{8}$", ErrorMessage = "0 followed by 8 digits")]
+        public string StudentNumber { get; set; }
+    }
 
-  public class StudentFull : StudentsForList {
-    [Required]
-    [StringLength(40, MinimumLength = 3)]
-    [Display(Name = "First Name")]
-    public string FirstName { get; set; }
+    public class StudentFull : StudentsForList
+    {
+        public StudentFull()
+        {
+            this.Courses = new List<CourseForList>();
+        }
+        [Required]
+        [StringLength(40, MinimumLength = 3)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
 
-    [Required]
-    [StringLength(100, MinimumLength = 3)]
-    [Display(Name = "Last Name")]
-    public string LastName { get; set; }
-    [Required]
-    [RegularExpression("^[2-9]\\d{2}-\\d{3}-\\d{4}$", ErrorMessage = "nnn-nnn-nnnn")]
-    public string Phone { get; set; }
-  }
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [RegularExpression("^[2-9]\\d{2}-\\d{3}-\\d{4}$", ErrorMessage = "nnn-nnn-nnnn")]
+        public string Phone { get; set; }
+        public List<CourseForList> Courses { get; set; }
+    }
 
-  public class StudentPublic {
+    public class StudentPublic
+    {
+        [Key]
+        public int StudentId { get; set; }
 
-    [Required]
-    [RegularExpression("^[0][0-9]{8}$", ErrorMessage = "0 followed by 8 digits")]
-    public string StudentNumber { get; set; }
+        [Required]
+        [RegularExpression("^[0][0-9]{8}$", ErrorMessage = "0 followed by 8 digits")]
+        public string StudentNumber { get; set; }
 
-    [Required]
-    [StringLength(40, MinimumLength = 3)]
-    [Display(Name = "First Name")]
-    public string FirstName { get; set; }
+        [Required]
+        [StringLength(40, MinimumLength = 3)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
 
-    [Required]
-    [StringLength(100, MinimumLength = 3)]
-    [Display(Name = "Last Name")]
-    public string LastName { get; set; }
-    [Required]
-    [RegularExpression("^[2-9]\\d{2}-\\d{3}-\\d{4}$", ErrorMessage = "nnn-nnn-nnnn")]
-    public string Phone { get; set; }
-  }
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [RegularExpression("^[2-9]\\d{2}-\\d{3}-\\d{4}$", ErrorMessage = "nnn-nnn-nnnn")]
+        public string Phone { get; set; }
+    }
+
+    public class StudentName
+    {
+        [Key]
+        public int StudentId { get; set; }
+
+        [Required]
+        [StringLength(40, MinimumLength = 3)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+    }
 }
