28b8c54f33b351a18a2a60806d6d8137f0750c14
igoryen <igor.yentaltsev@gmail.com>
2014-02-06 11:36:08 -0500

    ViewModels/Repo_Courses.cs: added from seed file

---
+++ myAppMemory/ViewModels/Repo_Courses.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstOne.ViewModels
+{
+    public class Repo_Courses : RepositoryBase
+    {
+        public static List<CourseForList> getCourseForList(List<CodeFirstOne.Models.Course> ls)
+        {
+            List<CourseForList> nls = new List<CourseForList>();
+            foreach (var item in ls)
+            {
+                CourseForList co = new CourseForList();
+                co.CourseCode = item.CourseCode;
+                co.CourseId = item.Id;
+                nls.Add(co);
+            }
+
+            return nls;
+        }
+    }
+}
