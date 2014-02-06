7f30c0741cadcee9729ba9d5238547797cc2580a
igoryen <igor.yentaltsev@gmail.com>
2014-02-06 11:22:12 -0500

    Controllers/HomeController.cs: changed app name back

--- myAppMemory/Controllers/HomeController.cs
+++ myAppMemory/Controllers/HomeController.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using CodeFirstOne.Models;
-using CodeFirstOne.ViewModels;
+using MyAppMemory.Models;
+using MyAppMemory.ViewModels;
 
 namespace CodeFirstOne.Controllers
 {
